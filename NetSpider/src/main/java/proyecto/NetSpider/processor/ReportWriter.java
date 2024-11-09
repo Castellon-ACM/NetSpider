6c2791bf01ca4b04d3b3ddddc691094bd911fb2d
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-11-09 17:39:50 +0100

    ReportWriter started

--- NetSpider/src/main/java/proyecto/NetSpider/processor/ReportWriter.java
+++ NetSpider/src/main/java/proyecto/NetSpider/processor/ReportWriter.java
@@ -1,4 +1,37 @@
 package proyecto.NetSpider.processor;
 
-public class ReportWriter {
+import proyecto.NetSpider.entities.Node;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReportWriter extends Thread {
+
+    private ArrayList<Node> nodes = new ArrayList<>();
+    private File file;
+
+    /**
+     * Write the scan results into a file asynchronously
+     * @param nodes List of a nodes to write
+     * @param file Destination file
+     */
+    public ReportWriter(ArrayList<Node> nodes, File file) {
+        this.nodes = nodes;
+        this.file = file;
+        if (!nodes.isEmpty()) this.start();
+    }
+
+    @Override
+    public void run() {
+        try (FileWriter writer = new FileWriter(file)) {
+            for (Node node : nodes) {
+                // IMPLEMENTAR ESCRITURA DE RESULTADOS AQUI
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
