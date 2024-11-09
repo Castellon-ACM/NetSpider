5ca744d32656c5711543404045b9028e8ef32cc3
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-11-09 17:20:01 +0100

    RawIpWriter completed

--- NetSpider/src/main/java/proyecto/NetSpider/processor/RawIpWriter.java
+++ NetSpider/src/main/java/proyecto/NetSpider/processor/RawIpWriter.java
@@ -2,22 +2,28 @@ package proyecto.NetSpider.processor;
 
 import proyecto.NetSpider.entities.Node;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RawIpWriter {
-    private ArrayList<String> rawIps = new ArrayList<>();
-
     /**
-     * Write a new file of node
-     * @param nodes
+     * Writes a file with the nodes ips
+     * @param nodes Node object
+     * @param file Destination file
+     * @param regex Regex to separate the ips
      */
-    public RawIpWriter(ArrayList<Node> nodes) {
-        if (!nodes.isEmpty()) {
-            for (Node node : nodes) {
-                rawIps.add(node.getIp());
+    public static void IpWriter(ArrayList<Node> nodes, File file , char regex) {
+        try (FileWriter writer = new FileWriter(file)){
+            if (!nodes.isEmpty()) {
+                for (Node node : nodes) {
+                    writer.write(node.getIp() + regex);
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
     }
 
 }
