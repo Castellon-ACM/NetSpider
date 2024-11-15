2f0c57a70646b121d8fe96720a99088b64febb05
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-11-15 22:23:06 +0100

    Equilibrator Structure defined

--- NetSpider/src/main/proyecto/NetSpider/equilibrator/Equilibrator.java
+++ NetSpider/src/main/proyecto/NetSpider/equilibrator/Equilibrator.java
@@ -3,14 +3,24 @@ package main.proyecto.NetSpider.equilibrator;
 import main.proyecto.NetSpider.data_controller.Migrable;
 import main.proyecto.NetSpider.entities.Node;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Equilibrator implements Migrable {
-    private CopyOnWriteArrayList<Node> nodes = new CopyOnWriteArrayList<>();
+
+    private final File PythonScript = new File("./python_modules/scanner.py");
+
+    // QUEUE OF NODES TO PROCESS
+    private CopyOnWriteArrayList<Node> ProcessQueue = new CopyOnWriteArrayList<>();
+
+    // QUEUE OF PROCESSED NODES
+    private CopyOnWriteArrayList<Node> ProcessedQueue = new CopyOnWriteArrayList<>();
 
 
+    public void ProcessIps() {
 
+    }
 
 
     /**
