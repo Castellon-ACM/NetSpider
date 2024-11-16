00293fd9bc939742d7f3355257fe92c90a589956
felix-caba <felixcaba@proton.me>
2024-11-16 10:09:42 +0100

    Implementation of Singleton Class Pattern on the Configuration.

--- NetSpider/src/main/proyecto/NetSpider/config/Configuration.java
+++ NetSpider/src/main/proyecto/NetSpider/config/Configuration.java
@@ -1,12 +1,50 @@
 package main.proyecto.NetSpider.config;
 // CONFIGURACIÓN DEL PROYECTO
 
-// TODO Singleton
-
 public class Configuration {
-    // Expiration time of the scanned nodes in seconds
-    public static int expirationTime = 50;
-    public static boolean verboseMode = true;
 
-    public static int processesVolume = 20;
+    private static Configuration instance = null;
+
+    private int expirationTime;
+    private boolean verboseMode;
+    private int processesVolume;
+
+    private Configuration() {
+        this.expirationTime = 50;
+        this.verboseMode = true;
+        this.processesVolume = 20;
+    }
+
+    public static Configuration getInstance() {
+        if (instance == null) {
+            instance = new Configuration();
+        }
+        return instance;
+    }
+
+    public int getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(int expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+
+    public boolean isVerboseMode() {
+        return verboseMode;
+    }
+
+    public void setVerboseMode(boolean verboseMode) {
+        this.verboseMode = verboseMode;
+    }
+
+    public int getProcessesVolume() {
+        return processesVolume;
+    }
+
+    public void setProcessesVolume(int processesVolume) {
+        this.processesVolume = processesVolume;
+    }
+
+  
 }
