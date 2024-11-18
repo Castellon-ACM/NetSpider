7d62eb0a96b1f5da0d186759a90faa46cd4058a9
CASTELLON13 <alexcastellon077@gmail.com>
2024-11-18 21:24:22 +0100

    Create readNodesFromXML to read the XML created in ReportWriter.

--- NetSpider/src/processor/XMLToPDF.java
+++ NetSpider/src/processor/XMLToPDF.java
@@ -1,5 +1,40 @@
 package processor;
 
-public class XMLToPDF {
+import entities.Node;
+import entities.Port;
+import entities.Service;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.util.ArrayList;
+
+public class XMLToPDF {
+    private File xmlFile;
+
+    public XMLToPDF(File xmlFile, File pdfFile) {
+        this.xmlFile = xmlFile;
+
+    }
+
+    private ArrayList<Node> readNodesFromXML() throws JAXBException {
+        ArrayList<Node> nodes = new ArrayList<>();
+        try (FileInputStream inputStream = new FileInputStream(xmlFile)) {
+
+            JAXBContext context = JAXBContext.newInstance(Node.class, Port.class, Service.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            Node node = (Node) unmarshaller.unmarshal(inputStream);
+            nodes.add(node);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al leer el XML: " + e.getMessage(), e);
+        }
+        return nodes;
+    }
 }
+
